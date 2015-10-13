4c152bc67489ce353805ed57d7f9bace17803493
Carina Alvarez <cfs@qad.com>
2015-10-13 20:03:21 +0000

    Removing logic to create .xml files, adding logic to check LD status - Changes made by Carina Alvarez (cfs@qad.com).

--- sample-barcode-scanner/scripts/main.js
+++ sample-barcode-scanner/scripts/main.js
@@ -11,7 +11,6 @@ function App() {
 
 App.prototype = {
     resultsField: null,
-	fileSystemHelper: null,
      
     run: function() {
         var that = this,
@@ -35,7 +34,6 @@ App.prototype = {
             cordova.plugins.barcodeScanner.scan(
                 function(result) {
                     if (!result.cancelled) {
-                        //that._addMessageToLog(result.format + " | " + result.text);    
                         that._parseLegalDocNumber(result.text);
                     }
                 }, 
@@ -59,90 +57,49 @@ App.prototype = {
     },
     
     _parseLegalDocNumber: function (message) {
-        var that = this;        
+        var that = this,
+            status = "",
+            suggestedcontent = "",
+            ldnumber = "";
         
         // Display Result
         if (message.length == 44) {
-			that._createAndSendFile(message);
-            message = message.substring(25, 34) + message.substring(22, 25);
-            that._addMessageToLog("Legal Document Number: " + message);
+            ldnumber = message.substring(25, 34) + message.substring(22, 25);
+            
+            //Check NFe Key Code status
+            switch (ldnumber)
+            {
+               case "748113081001": 
+                   status = "authorized";
+                   break;
+                    
+                case "000000313005":
+                    status = "rejected";
+                    break;
+
+               default: 
+                   status = "rejected";
+                   break;
+            }
+            
+			//Add result to app form
+            message = ldnumber + "</br>Status: " + status;
+            that._addMessageToLog("Legal Document Number: " + message);            
+            
+            //Ask user if wants to send e-mail to Supplier
+            if (confirm("The Legal Document Number " + ldnumber + " is " + status + ". Notify Supplier?")){
+                //Open e-mail app with suggested text 
+                if (status = "authorized"){
+					suggestedcontent = "The Legal Document Number " + ldnumber + " was received in our dock and will be processed";   
+                }
+                else {
+					suggestedcontent = "The Legal Document Number " + ldnumber + " is not authorized. Good will be returned.";
+                }                
+            }                     
+            
         } else {
             that._addMessageToLog("Incorrect code length: " + message.length);
         }
-    },
-    
-    _createAndSendFile: function (message){
-        var that = this;
-        var filecontent;
-        var filename;
-        var dataURI;
-        
-        filecontent = '<?xml version="1.0" encoding="UTF-8" ?>' + 
-        			  '<soap12:Envelope xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:soap12="http://www.w3.org/2003/05/soap-envelope">' + 
-            		  '<soap12:Header>' +
-        			  '<nfeCabecMsg xmlns="http://www.portalfiscal.inf.br/nfe/wsdl/NfeConsulta2">' +
-                      '<cUF>35</cUF>' +
-            		  '<versaoDados>3.10</versaoDados>' +
-            		  '</nfeCabecMsg>' +
-            		  '</soap12:Header>' +
-            		  '<soap12:Body>' +
-            		  '<nfeDadosMsg xmlns="http://www.portalfiscal.inf.br/nfe/wsdl/NfeConsulta2">' +
-            		  '<consSitNFe versao="3.10" xmlns="http://www.portalfiscal.inf.br/nfe">' +
-            	      '<tpAmb>2</tpAmb>' +
-            	      '<xServ>CONSULTAR</xServ>' +
-            		  '<chNFe>' + message + '</chNFe>' +
-            	      '</consSitNFe>' +
-            		  '</nfeDadosMsg>' +
-            		  '</soap12:Body>' +
-            		  '</soap12:Envelope>';
-        
-        filename = message + ".xml";        
-        //fileSystemHelper.deleteFile(filename, that._onSuccess, that._onError);
-		fileSystemHelper.writeLine(filename, filecontent, that._onSuccess(filename), that._onError);
-        
-    },
-    
-	_onSuccess: function(filename) {     
-        
-        //If file creation was succesful, then transmit file to SEFAZ
-        var that = this,
-        	options = new FileUploadOptions(),     
-            fileURI = filename,
-            filePath = "",
-			up = new FileTransfer();
-        
-		alert("File created: " + filename);          
-        
-        options.fileKey = filename;       
-        options.fileName = fileURI.substr(fileURI.lastIndexOf('/') + 1);        
-        options.mimeType = "application/xml";
-        options.params = {}; // if we need to send parameters to the server request 
-        //options.headers = {
-          //  Connection: "Close"
-        //};        
-		options.chunkedMode = false;        
-
-        up.upload(
-            fileURI,
-            encodeURI("http://www.portalfiscal.inf.br/nfe/wsdl/NfeConsulta2"),
-            that._onFileUploadSuccess,
-            that._onFileTransferFail,
-            options);           
-        
-	},
-    
-	_onError: function(error) {
-        //file creation error
-		alert(error.message);
-	}, 
-    
-    _onFileUploadSucces: function(){
-        alert("File sent!");
-    },
-    
-    _onFileTransferFail: function(error){
-        //file transfer error
-        alert(error.code + " " + error.source + " " + error.target);
-    }
+    }    
               
 } 
